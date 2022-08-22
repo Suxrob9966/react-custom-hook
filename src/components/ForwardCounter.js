3e9bae9daea813258163c88fed4eb8b00dc4d548
Пользователь <rabbimovich9966@gmail.com>
2022-08-21 19:18:58 -0500

    added comment

--- src/components/ForwardCounter.js
+++ src/components/ForwardCounter.js
@@ -3,11 +3,9 @@ import useCounter from "../hooks/use-counter";
 import Card from "./Card";
 
 const ForwardCounter = () => {
-  const counter = useCounter();
+  const counter = useCounter(); // we don't need to pass forwards parameter as it is default parameter in us-counter hook
 
   return <Card>{counter}</Card>;
 };
 
 export default ForwardCounter;
-
-// some comment
